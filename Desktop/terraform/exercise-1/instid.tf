a4f4ea5c10022a777c53c39722795876d4c38fa6
pvedha24 <pvedhadivyasri@gmail.com>
2025-06-26 15:05:53 +0530

    Clean commit without .terraform directory

---
+++ Desktop/terraform/exercise-1/instid.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "amiID" {
+  most_recent = true
+
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+output "instance_id" {
+  description = "value of the instance id"
+  value       = data.aws_ami.amiID.id
+}
---
+++ Desktop/terraform/exercise-2/Keypair.tf
@@ -0,0 +1,6 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dove-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwlOW/i+UeMxqeePJmBFgDpg446qR6/FC4Eh3P0sJ87 vedhadivyasri@vedhas-MacBook-Air.local"
+}
+
+
